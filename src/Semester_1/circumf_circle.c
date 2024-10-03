167dfaccd4eddba35ad7bf71428a8d8a434313ba
Amrit9304 <91752236+Amrit9304@users.noreply.github.com>
2024-10-03 11:55:20 +0530

    Add files via upload

---
+++ src/Semester_1/circumf_circle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+    float area, circum, radius;
+    printf("Enter the radius of a circle (in cm) : ");
+    scanf("%f",&radius);
+    
+    area = 2*PI*radius;
+    printf("The area of a circle is : %.2f\n", area);
+    
+    circum = PI*radius*radius;
+	printf("The circumference of a circle is : %.2f", circum);
+	
+	return 0;
+}
